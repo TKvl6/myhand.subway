920208f905c115e0e2c2a1c170133ff5f4ed1d31
James-Baek <ckdgus1428@gmail.com>
2019-10-14 00:47:52 +0900

    json 파싱

--- playground.subway/code/MenuFind.js
+++ playground.subway/code/MenuFind.js
@@ -10,19 +10,37 @@ module.exports.function = function menuFind (menu) {
   const http = require('http');
   const console = require('console');
   let response = http.getUrl('https://api.sheety.co/b22d0d25-9ba9-481b-a000-a2206781526b', options);
-  console.log(response[0].kname);
+  
 
+  console.log(response[0].material);
+  
   var result = [];
   console.log(String(menu).replace(" ",""));
-  var material = "";
+
+  var aJsonArray = new Array();
+
+  var li = response[0].material.split(",");
+  li.forEach(function(el){
+    var aJson = new Object();
+    aJson.mt = el;
+    console.log(el);
+    aJsonArray.push(aJson);
+  })
+  
+  console.log(aJsonArray.pop);
+
+  // var material = JSON.stringify(aJsonArray);
+
   for(var i = 0; i < response.length; i++) {
 
     if(textLib.fuzzyMatch(response[i].kname, menu)){
       result.push(response[i]);
     }
   }
-  console.log(result);
+  // result[0].material = material;
 
+  console.log(result);
+  console.log("첫번째 재료 : " + result[0].material[0].mt);
   // RollResult
   return result;
 }
