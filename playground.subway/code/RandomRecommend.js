e5e1ec757dc466c2d01446ae45bf836c342105aa
leegeunseong <skscp223@gmail.com>
2019-10-15 23:49:52 +0900

    랜덤 추천 추가

---
+++ playground.subway/code/RandomRecommend.js
@@ -0,0 +1,17 @@
+module.exports.function = function randomRecommend () {
+  var textLib = require('textLib');
+  var tool = require('tool/util.js');
+  var console = require('console');
+  let response = tool.getSandwichList();
+  
+  var result = [];
+  for(var i = 0; i < response.length; i++) {
+    var roll = Math.ceil(Math.random() * 10);
+    console.log(roll);
+    response[i].material = tool.divideMaterial(response[i].material);
+    if(roll == 5)
+      result.push(response[i]);
+  }
+
+  return result;
+}
