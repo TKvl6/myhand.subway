be13609615478c2e3d64e9fd1265fc346fd5b2af
leegeunseong <skscp223@gmail.com>
2019-10-16 19:58:06 +0900

    왜안돼git add .git add .git add .git add .

--- playground.subway/code/tool/util.js
+++ playground.subway/code/tool/util.js
@@ -13,7 +13,6 @@ module.exports.searchAllergyByName = function(aName){
 }
 module.exports.getSandwichList = function(){
   let sandwich = db.getSandwich();
-  
   return sandwich;
 }
 module.exports.searchSandwichByName = function(kName){
@@ -55,18 +54,6 @@ module.exports.searchSandwichByCal = function(cal, standard){
       }
     }
   }
-  return result;
-}
-module.exports.searchSandwichByTag = function(tag){ 
-  // standard : cal보다 높은지 낮은지
-  let sandwich = db.getSandwich();
-  let result = [];
-  for(i = 0; i < sandwich.length; i++){
-    if(textLib.fuzzyMatch(sandwich[i].tag,tag)){
-      result.push(sandwich[i]);
-    }
-  }
-  return result;
 }
 module.exports.divideMaterial = function(m){
   var aJsonArray = new Array();
@@ -87,4 +74,73 @@ module.exports.divideTag = function(tag){
   }
   return str;
 }
-
+module.exports.tagsentence = function(material) {
+  var http = require('http');
+  var textLib = require('textLib');
+  var console = require('console');
+  var tag = http.getUrl('https://api.sheety.co/e543fd14-622d-46cf-a993-7654aa4a22be',{format : "json"});
+  var res = [];
+  var regex = "0-9";
+  for(i = 0; i < material.length; i++){
+    var len = material[i].mt.split(" ").length;
+    var mtArr;
+    if(material[i].mt.split(" ")[0] == "추천"){
+      for(i = 1; i < len; i++){
+        mtArr = mtArr + material[i].mt.split(" ")[i];
+      }
+    }else if(len > 1) mtArr = material[i].mt.split(" ",len-1);
+    else if(len == 1) mtArr = material[i].mt
+    console.log(mtArr);
+    console.log(mtArr[0]);
+    if(len > 1 && mtArr[0] != "추천") mtArr = mtArr.join("");
+    console.log(mtArr)
+    next: for(j = 0; j < tag.length; j++){
+      // console.log(mtArr + " / " + tag[j].material)
+      if(textLib.fuzzyMatch(mtArr,tag[j].material)){
+        for(k = 0; k < res.length; k++){
+          if(textLib.fuzzyMatch(res[k].tag,mtArr)){
+            for(l = 1; l <= 6; l++){
+              var index = "idx" + l;
+              console.log(index);
+              if(tag[j].index != null){
+                var tmp = parseInt(res[k].index) + parseInt(tag[j].index);
+                res[k].index = tmp;
+              }
+            }
+            continue next;
+          }
+        }
+        res.push({ tag : mtArr, idx1 : tag[j].idx1
+                , idx2 : tag[j].idx2, idx3 : tag[j].idx3
+                , idx4 : tag[j].idx4, idx5 : tag[j].idx5
+                , idx6 : tag[j].idx6});
+      //  res.push({ tag : mtArr[0], spicy : tag[j].idx1
+      //           , sweet : tag[j].idx2, salty : tag[j].idx3
+      //           , sour : tag[j].idx4, juicy : tag[j].idx5
+      //           , tasty : tag[j].idx6});
+        console.log(res);
+      }
+    }
+  }
+  console.log(res);
+  var str =  ["환상적인","환상적인","환상적인","환상적인"];
+  var roll = Math.ceil(Math.random * str.length);
+  var maxScore = 0;
+  var maxTag;
+  var suffix = "한 맛";
+  for(i = 0; i < res.length; i++){
+    for(j=1; j <= 6; j++)
+    var index = "idx" + j;
+    if(res[i].index > maxScore){
+      maxScore = res[i].index;
+      if(j == 1) maxTag = "매콤";
+      else if(j == 2) maxTag = "달콤";
+      else if(j == 3) maxTag = "짭짤";
+      else if(j == 4) maxTag = "새콤"
+      else if(j == 5) maxTag = "육즙"
+      else if(j == 6) maxTag = "고소"
+    }
+  }
+  var result = str[roll] + maxTag + suffix;
+  return result;
+}
