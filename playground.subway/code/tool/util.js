9f1824b402b07969087d4bb3676a83c909000513
leegeunseong <skscp223@gmail.com>
2019-10-17 10:19:37 +0900

    환상적인~~

--- playground.subway/code/tool/util.js
+++ playground.subway/code/tool/util.js
@@ -83,64 +83,65 @@ module.exports.tagsentence = function(material) {
   var regex = "0-9";
   for(i = 0; i < material.length; i++){
     var len = material[i].mt.split(" ").length;
-    var mtArr;
-    if(material[i].mt.split(" ")[0] == "추천"){
-      for(i = 1; i < len; i++){
-        mtArr = mtArr + material[i].mt.split(" ")[i];
-      }
-    }else if(len > 1) mtArr = material[i].mt.split(" ",len-1);
+    var mtArr = "";
+    if(material[i].mt.split(" ")[0] == "추천") for(j = 1; j < len; j++) mtArr = mtArr + material[i].mt.split(" ")[j];
+    else if(len > 1){
+      mtArr = material[i].mt.split(" ",len-1);
+      mtArr = mtArr.join(" ");
+    } 
     else if(len == 1) mtArr = material[i].mt
-    console.log(mtArr);
-    console.log(mtArr[0]);
-    if(len > 1 && mtArr[0] != "추천") mtArr = mtArr.join("");
-    console.log(mtArr)
     next: for(j = 0; j < tag.length; j++){
-      // console.log(mtArr + " / " + tag[j].material)
       if(textLib.fuzzyMatch(mtArr,tag[j].material)){
         for(k = 0; k < res.length; k++){
           if(textLib.fuzzyMatch(res[k].tag,mtArr)){
-            for(l = 1; l <= 6; l++){
-              var index = "idx" + l;
-              console.log(index);
-              if(tag[j].index != null){
-                var tmp = parseInt(res[k].index) + parseInt(tag[j].index);
-                res[k].index = tmp;
-              }
-            }
+            res[k].idx1 = parseInt(res[k].idx1) + parseInt(tag[j].idx1);
+            res[k].idx2 = parseInt(res[k].idx2) + parseInt(tag[j].idx2);
+            res[k].idx3 = parseInt(res[k].idx3) + parseInt(tag[j].idx3);
+            res[k].idx4 = parseInt(res[k].idx4) + parseInt(tag[j].idx4);
+            res[k].idx5 = parseInt(res[k].idx5) + parseInt(tag[j].idx5);
+            res[k].idx6 = parseInt(res[k].idx6) + parseInt(tag[j].idx6);
             continue next;
           }
         }
-        res.push({ tag : mtArr, idx1 : tag[j].idx1
-                , idx2 : tag[j].idx2, idx3 : tag[j].idx3
-                , idx4 : tag[j].idx4, idx5 : tag[j].idx5
-                , idx6 : tag[j].idx6});
-      //  res.push({ tag : mtArr[0], spicy : tag[j].idx1
-      //           , sweet : tag[j].idx2, salty : tag[j].idx3
-      //           , sour : tag[j].idx4, juicy : tag[j].idx5
-      //           , tasty : tag[j].idx6});
-        console.log(res);
+        res.push({ tag : mtArr, idx1 : tag[j].idx1, idx2 : tag[j].idx2
+                , idx3 : tag[j].idx3, idx4 : tag[j].idx4
+                , idx5 : tag[j].idx5, idx6 : tag[j].idx6});
+        break;
       }
     }
   }
-  console.log(res);
   var str =  ["환상적인","환상적인","환상적인","환상적인"];
-  var roll = Math.ceil(Math.random * str.length);
+  var roll = Math.ceil(Math.random() * str.length-1);
   var maxScore = 0;
-  var maxTag;
-  var suffix = "한 맛";
+  var maxTag = "";
+  var suffix = " 맛!";
+  var taste = [0,0,0,0,0,0];
+  var util = require('tool/util.js')
   for(i = 0; i < res.length; i++){
-    for(j=1; j <= 6; j++)
-    var index = "idx" + j;
-    if(res[i].index > maxScore){
-      maxScore = res[i].index;
-      if(j == 1) maxTag = "매콤";
-      else if(j == 2) maxTag = "달콤";
-      else if(j == 3) maxTag = "짭짤";
-      else if(j == 4) maxTag = "새콤"
-      else if(j == 5) maxTag = "육즙"
-      else if(j == 6) maxTag = "고소"
+    taste[0] += res[i].idx1
+    taste[1] += res[i].idx2
+    taste[2] += res[i].idx3
+    taste[3] += res[i].idx4
+    taste[4] += res[i].idx5
+    taste[5] += res[i].idx6
+  }
+  for(i=0; i < taste.length; i++){
+    if(taste[i] > maxScore){
+      maxScore = taste[i];
+      maxTag = util.tasteTag(i+1);
     }
   }
-  var result = str[roll] + maxTag + suffix;
+  var result = str[roll] + " " + maxTag + suffix;
   return result;
+}
+module.exports.tasteTag = function(index){
+  var maxTag;
+  if(index == 1) maxTag = "매콤한";
+  else if(index == 2) maxTag = "달콤한";
+  else if(index == 3) maxTag = "짭짤한";
+  else if(index == 4) maxTag = "새콤한"
+  else if(index == 5) maxTag = "육즙의"
+  else if(index == 6) maxTag = "고소한"
+
+  return maxTag;
 }
