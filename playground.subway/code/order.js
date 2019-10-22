c10a79c789513f6e5991d9b67860ea8527a89509
yourname <your@email.address>
2019-10-22 10:32:01 +0900

    order

---
+++ playground.subway/code/order.js
@@ -0,0 +1,7 @@
+module.exports.function = function order (menu) {
+  var console = require('console');
+  var textLib = require('textLib');
+  var util = require('tool/util.js')
+  var result = menu;
+  return result;
+}
